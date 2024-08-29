d3b4356b16d14184c552db15e5b6af265f904b62
Matija <48520233+MatGMC7@users.noreply.github.com>
2024-08-30 00:22:52 +0200

    added date and time in file name

--- wifi-passwords.ps1
+++ wifi-passwords.ps1
@@ -1,3 +1,5 @@
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-File pass.txt
-type pass.txt
+$Date = Get-Date -Format yyyy-MM-dd
+$Time = Get-Date -UFormat %H-%M-%S
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-File "pass $Date $Time.txt"
+type "pass $Date $Time.txt"
 pause
